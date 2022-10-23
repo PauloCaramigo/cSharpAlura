f6dbb7e7978bb3b33026541d192d1f421f9ea278
Paulo Caramigo <paulo.domingues.caramigo@gmail.com>
2022-10-23 16:16:21 -0300

    Foi feito o modulo 1 do curso e o desafio

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using ByteBank;
+
+ContaCorrente andre = new ContaCorrente();
+
+andre.titular = "André Silva";
+andre.conta = "1010-X";
+andre.numeroAgencia = 15;
+andre.saldo = 100.00;
+
+Console.WriteLine("Saldo da conta do André = " + andre.saldo);
---
+++ Desafios/SuperMercado_MenorPreco.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafios
+{
+    public class SuperMercado_MenorPreco
+    {
+        // nome, fabricante, código de barras, valor unitário, valor de compra e quantidade em estoque
+        public string nome_produto { get; set; }
+        public string fabricante { get; set; }
+        public int codigo { get; set; }
+        public double valor_unitario { get; set; }
+        public double valor_compra { get; set; }
+        public int quantidade_estoque { get; set; }
+    }
+}
